7b66cf16f2dddf95d3fee3c3905f36cee61ba3e1
emurciaf <emurciaf@hotmail.com>
2021-11-17 14:03:45 +0100

    ejercicios clase 3

--- CursoPython2021/clase3/FestivalMusical.py
+++ CursoPython2021/clase3/FestivalMusical.py
@@ -3,8 +3,21 @@ class FestivalMusical:
         self.nombre = nombre
         self.pais = pais
         self.estilo_musical = estilo_musical
+    def festival_metodo(self):
+        print('El mejor festival es:')
+
+
 festival1 = FestivalMusical('Creamfileds', 'UK', 'Dance')
 festival2 = FestivalMusical('Primavera Sound', 'SP', 'Dance')
 
 print( festival1.nombre, festival1.pais, festival1.estilo_musical)
 print( festival2.nombre, festival2.pais, festival2.estilo_musical)
+
+festival2.festival_metodo()
+print(festival2.nombre.upper())
+
+festival1.nombre = 'Benicassim'
+print( festival1.nombre)
+
+del festival1
+print( festival1.nombre)
